e031b3740ca876eb41fdb64afd654c8e9aa120e0
lukasz-kukulka <qqla83@gmail.com>
2021-05-14 13:15:29 +0100

    fixup! Homework generate sequence

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -4,9 +4,8 @@
 
 std::vector<int> generateSequence(int count, int step) {
     if (count <= 0){
-        std::vector<int> vec;
-        return vec;
-    } else {
+        return {};
+    }
     std::vector<int> vec(count);
     auto results = 0;
     std::generate(vec.begin(), vec.end(), [&step, &results]() { 
@@ -14,5 +13,4 @@ std::vector<int> generateSequence(int count, int step) {
         return results; 
     });
     return vec;
-    }
 }
